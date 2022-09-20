bea6cee7b9d14a5eb1eedcac32d2ff0cbb2d6f00
nanvon <nanvon@foxmail.com>
2022-09-20 11:16:58 +0800

    ws

--- recorder.js
+++ recorder.js
@@ -1,43 +1,45 @@
-const recordBtn = document.querySelector(".record-btn");
-const player = document.querySelector(".audio-player");
+const recordBtn = document.querySelector('.record-btn');
+const player = document.querySelector('.audio-player');
 
 if (navigator.mediaDevices.getUserMedia) {
   var chunks = [];
   const constraints = { audio: true };
   navigator.mediaDevices.getUserMedia(constraints).then(
-    stream => {
-      console.log("授权成功！");
+    (stream) => {
+      console.log('授权成功！');
 
       const mediaRecorder = new MediaRecorder(stream);
 
       recordBtn.onclick = () => {
-        if (mediaRecorder.state === "recording") {
+        if (mediaRecorder.state === 'recording') {
           mediaRecorder.stop();
-          recordBtn.textContent = "record";
-          console.log("录音结束");
+          recordBtn.textContent = 'record';
+          console.log('录音结束');
         } else {
           mediaRecorder.start();
-          console.log("录音中...");
-          recordBtn.textContent = "stop";
+          console.log('录音中...');
+          recordBtn.textContent = 'stop';
         }
-        console.log("录音器状态：", mediaRecorder.state);
+        console.log('录音器状态：', mediaRecorder.state);
       };
 
-      mediaRecorder.ondataavailable = e => {
+      mediaRecorder.ondataavailable = (e) => {
         chunks.push(e.data);
+        //ws发送录屏切片
+        //...
       };
 
-      mediaRecorder.onstop = e => {
-        var blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+      mediaRecorder.onstop = (e) => {
+        var blob = new Blob(chunks, { type: 'audio/ogg; codecs=opus' });
         chunks = [];
         var audioURL = window.URL.createObjectURL(blob);
         player.src = audioURL;
       };
     },
     () => {
-      console.error("授权失败！");
+      console.error('授权失败！');
     }
   );
 } else {
-  console.error("浏览器不支持 getUserMedia");
+  console.error('浏览器不支持 getUserMedia');
 }
